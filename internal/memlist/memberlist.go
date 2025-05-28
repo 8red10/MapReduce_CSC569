02d62d66c0b123bee3954691911d4b5800e32c72
Jack Krammer <jackkrammer@jfire.lan>
2025-05-28 11:30:36 -0700

    add memlist API

---
+++ internal/memlist/memberlist.go
@@ -0,0 +1,79 @@
+package memlist
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/8red10/MapReduce_CSC569/internal/node"
+)
+
+/* MemberList struct represents participanting nodes - holds Node information */
+type MemberList struct {
+	mu      *sync.RWMutex     // enables thread-safe operations
+	Members map[int]node.Node // holds node info
+}
+
+/* Returns a new instance of a MemberList (returns as a pointer) */
+func NewMemberList() *MemberList {
+	return &MemberList{
+		mu:      new(sync.RWMutex),
+		Members: make(map[int]node.Node),
+	}
+}
+
+/* Adds (replaces) a node to the member list - method of MemberList - RPC ok */
+func (m *MemberList) Add(payload node.Node, reply *node.Node) error {
+
+	/* Check the input node's ID is within proper range */
+	if payload.ID < 1 || payload.ID > node.NUM_NODES {
+		return errors.New("MemberList.Add(): node ID out of range")
+	}
+
+	/* Add node to member list, replace existing node at ID if applicable */
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.Members[payload.ID] = payload
+	*reply = payload
+
+	/* Indicate success */
+	return nil
+}
+
+/* Updates a node in the member list - method of MemberList - RPC ok */
+func (m *MemberList) Update(payload node.Node, reply *node.Node) error {
+
+	/* Check ID is within range */
+	if payload.ID < 1 || payload.ID > node.NUM_NODES {
+		return errors.New("MemberList.Update(): node ID out of range")
+	}
+
+	/* Update node info at ID, adds node to ID entry if no node exists */
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.Members[payload.ID] = payload
+	*reply = payload
+
+	/* Indicate success */
+	return nil
+}
+
+/* Returns a node with specified ID - method of MemberList - RPC ok */
+func (m *MemberList) Get(payload int, reply *node.Node) error {
+
+	/* Check ID is within range */
+	if payload < 1 || payload > node.NUM_NODES {
+		return errors.New("MemberList.Get(): node ID out of range")
+	}
+
+	/* Return node with ID if exists in member list */
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	if node, exists := m.Members[payload]; exists {
+		*reply = node
+	} else {
+		return errors.New("MemberList.Get(): node with specified ID doesn't exist in member list")
+	}
+
+	/* Indicate success */
+	return nil
+}

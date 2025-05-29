598bdba8b6252c1a4107ddf2dee9550dd14b83c8
Jack Krammer <jackkrammer@jfire.lan>
2025-05-28 19:09:33 -0700

    update w package level timer

--- internal/timers/countvotes.go
+++ internal/timers/countvotes.go
@@ -13,47 +13,37 @@ const (
 	COUNT_VOTES_TIME = 50 // delay between candidate sending vote req and counting votes, in milliseconds
 )
 
-/*
-Creates a timer that can now be reset instead of initially started.
-Is so that this timer can be passed to a function instead of being returned from one.
-*/
-func CreateCountVotesTimer(server *rpc.Client, selfNode *node.Node, electionTimer *time.Timer) *time.Timer {
-	countVotesTimer := time.AfterFunc(
-		time.Millisecond*time.Duration(COUNT_VOTES_TIME),
-		// time.Second*time.Duration(3*COUNT_VOTES_TIME/10),
-		func() { CountVotesTimerCallback(server, selfNode, electionTimer) },
-	)
-	countVotesTimer.Stop()
-	return countVotesTimer
-}
+/* Package level variables */
+var CountVotesTimer *time.Timer
 
-func StartCountVotesTimer(server *rpc.Client, selfNode *node.Node, electionTimer *time.Timer) *time.Timer {
-	return time.AfterFunc(
+/* Creates the timer */
+func StartCountVotesTimer(server *rpc.Client, selfNode *node.Node) {
+	CountVotesTimer = time.AfterFunc(
 		time.Millisecond*time.Duration(COUNT_VOTES_TIME),
 		// time.Second*time.Duration(3*COUNT_VOTES_TIME/10),
-		func() { CountVotesTimerCallback(server, selfNode, electionTimer) },
+		func() { CountVotesTimerCallback(server, selfNode) },
 	)
 }
 
 /*
 Relies on the timer already being created.
 */
-func ResetCountVotesTimer(countVotesTimer *time.Timer) {
-	countVotesTimer.Reset(time.Millisecond * time.Duration(COUNT_VOTES_TIME))
+func ResetCountVotesTimer() {
+	CountVotesTimer.Reset(time.Millisecond * time.Duration(COUNT_VOTES_TIME))
 }
 
 /*
 Count the votes addressed to self during this term - part of candidate role.
 Can become a leader (only scenario to become a leader).
 */
-func CountVotesTimerCallback(server *rpc.Client, selfNode *node.Node, electionTimer *time.Timer) {
+func CountVotesTimerCallback(server *rpc.Client, selfNode *node.Node) {
 
 	fmt.Print("counting votes... ")
 	voteCount := msgs.CountVotes(server, selfNode.ID, selfNode.GetTerm())
 	fmt.Printf("got %d votes: ", voteCount)
 	if voteCount > node.NUM_NODES/2 { // shared.NUM_NODES/2+1
 		/* Case 1: candidate received vote from majority of nodes, become leader */
-		BecomeLeader(server, selfNode, electionTimer)
+		BecomeLeader(server, selfNode)
 	} else {
 		/* Case 2: candidate didn't receive majority vote, wait for next election timeout */
 		fmt.Println("not becoming leader")

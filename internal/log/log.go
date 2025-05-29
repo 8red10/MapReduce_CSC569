1fd65a6c9fc8a5cb9472acbc4b117cc4850452c5
Jack Krammer <jackkrammer@jfire.lan>
2025-05-29 06:39:04 -0700

    add package level var and logpending API

--- internal/log/log.go
+++ internal/log/log.go
@@ -5,9 +5,13 @@ import (
 	"sync"
 )
 
+/* Package level variable */
+var Selflog *Log
+
 type Log struct {
 	mu      *sync.RWMutex // enables thread-safe methods
 	Entries []LogEntry    // all log data
+	Pending []LogEntry    // entries pending the approval process
 	// Pending []LogEntry ? then can just loop through these pending to and add the matching one after LogMatchMessage
 	// 	or should LogMatchCounter have the current LogEntry
 	//	would then assume that could only have one pending LogEntry - this ok ?
@@ -20,16 +24,18 @@ type Log struct {
 }
 
 func NewLog() *Log {
-	entries := make([]LogEntry, 1, 10)
+	entries := make([]LogEntry, 0, 10)
+	pending := make([]LogEntry, 0, 10)
 
-	entries[0] = LogEntry{ // TODO - change this so that doesn't have 1 initially in log, just assumes that others will be able to check len via Log.GetSize()
-		Exists: false,
-		Index:  0,
-	}
+	// entries[0] = LogEntry{ // TODO - change this so that doesn't have 1 initially in log, just assumes that others will be able to check len via Log.GetSize()
+	// 	Exists: false,
+	// 	Index:  0,
+	// }
 
 	return &Log{
 		mu:      new(sync.RWMutex),
 		Entries: entries,
+		Pending: pending,
 	}
 }
 
@@ -88,4 +94,51 @@ func (l *Log) GetDeepCopy() []LogEntry {
 	copy(logCopy, l.Entries)
 
 	return logCopy
+}
+
+func (l *Log) GetPendingSize() int {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	return len(l.Pending)
+}
+
+func (l *Log) AddPending(entry LogEntry) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	index := len(l.Pending)
+	entry.Index = index
+	l.Pending = append(l.Pending, entry)
+
+	return nil
+}
+
+func (l *Log) UpdatePending(entry LogEntry, index int) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if s := len(l.Pending); index >= 0 && index < s {
+		l.Pending[index] = entry
+	} else {
+		return errors.New("Log.UpdatePending(): index out of range")
+	}
+
+	return nil
+}
+
+func (l *Log) GetPendingEntry(index int) LogEntry {
+	temp := LogEntry{
+		Exists: false,
+		Index:  -1,
+	}
+
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	if s := len(l.Pending); index >= 0 && index < s {
+		temp = l.Pending[index]
+	}
+
+	return temp
 }
